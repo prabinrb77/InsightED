b668a8b2f36982ba75394c4aa44207734834a85d
prabinrb <20033159@students.koi.edu.au>
2026-07-23 23:18:32 +1000

    changes

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  // GitHub Pages serves the site from /InsightED/ instead of the domain root
-  base: process.env.GITHUB_PAGES ? "/InsightED/" : "/",
-  plugins: [react()],
-});
